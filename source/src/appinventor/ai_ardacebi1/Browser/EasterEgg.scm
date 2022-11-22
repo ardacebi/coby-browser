911f8c7586884a0ab28873b1dbb92b5fc2ea0f26
Arda Çebi <ardacebi@me.com>
2022-11-22 15:33:56 +0300

    init

--- source/src/appinventor/ai_ardacebi1/Browser/EasterEgg.scm
+++
@@ -1,4 +0,0 @@
-#|
-$JSON
-{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"167","Source":"Form","Properties":{"$Name":"EasterEgg","$Type":"Form","$Version":"23","AppName":"Browser","BackgroundColor":"&HFF000000","ScreenOrientation":"portrait","Title":"EasterEgg","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"Clock1","$Type":"Clock","$Version":"3","TimerInterval":"1","Uuid":"-360911621"}]}}
-|#
